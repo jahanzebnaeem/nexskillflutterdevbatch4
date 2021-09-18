3c69ab392df488ece47ccae20c27cfe20810207a
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2021-09-18 22:58:14 +0500

    Lecture 3
    Working with single child and multi children widgets.

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -2,11 +2,41 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: 100,
+                color: Colors.white,
+                child: Text('Container 1'),
+              ),
+              Container(
+                height: 100,
+                color: Colors.red,
+                child: Text('Container 2'),
+              ),
+              Container(
+                height: 100,
+                color: Colors.blue,
+                child: Text('Container 3'),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
