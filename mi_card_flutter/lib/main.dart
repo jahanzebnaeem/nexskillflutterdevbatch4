5bf6f0c5b19837b2622d22fcfee028e6703da455
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2021-09-20 05:32:52 +0500

    Lecture 4
    Complete visiting card app.

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -16,23 +16,71 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                height: 100,
-                color: Colors.white,
-                child: Text('Container 1'),
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/jahanzeb.png'),
               ),
-              Container(
-                height: 100,
-                color: Colors.red,
-                child: Text('Container 2'),
+              Text(
+                'Jahanzeb Naeem',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40.0,
+                  fontFamily: 'Pacifico',
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              Container(
-                height: 100,
-                color: Colors.blue,
-                child: Text('Container 3'),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.0,
+                ),
               ),
+              SizedBox(
+                height: 10.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              const Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                    size: 20.0,
+                  ),
+                  title: Text(
+                    '555-555-5555',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.teal,
+                    ),
+                  ),
+                ),
+              ),
+              const Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                    size: 20.0,
+                  ),
+                  title: Text(
+                    'email@email.com',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.teal,
+                    ),
+                  ),
+                ),
+              )
             ],
           ),
         ),
