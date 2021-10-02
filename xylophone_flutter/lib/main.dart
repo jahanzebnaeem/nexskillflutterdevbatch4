9f962a59157007e342a38995a1334f1060b58110
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2021-10-02 13:33:39 +0500

    Lecture 7
    Music app complete

--- xylophone_flutter/lib/main.dart
+++ xylophone_flutter/lib/main.dart
@@ -1,16 +1,46 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(XylophoneApp());
 }
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded createButton({required Color color, required int soundNumber}) {
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(backgroundColor: color),
+        child: const Text(''),
+        onPressed: () {
+          playSound(soundNumber);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              createButton(color: Colors.red, soundNumber: 1),
+              createButton(color: Colors.orange, soundNumber: 2),
+              createButton(color: Colors.yellow, soundNumber: 3),
+              createButton(color: Colors.green, soundNumber: 4),
+              createButton(color: Colors.teal, soundNumber: 5),
+              createButton(color: Colors.blue, soundNumber: 6),
+              createButton(color: Colors.purple, soundNumber: 7),
+            ],
+          ),
         ),
       ),
     );
